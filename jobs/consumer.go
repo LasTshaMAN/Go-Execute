72c55062d113e9dd39dfb9fe32ae673845eff86a
Yury <sidorenkoy@restream.rt.ru>
2019-03-26 12:18:51 +0300

    Simplifying  structure.

--- jobs/consumer.go
+++ jobs/consumer.go
@@ -5,14 +5,8 @@ func Consume(jobQueue <-chan func()) {
 		panic("can't consume nil job queue")
 	}
 	go func() {
-		for {
-			select {
-			case function, ok := <-jobQueue:
-				if !ok {
-					return
-				}
-				function()
-			}
+		for function := range jobQueue {
+			function()
 		}
 	}()
 }
