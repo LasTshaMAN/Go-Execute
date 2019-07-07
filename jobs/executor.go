e35547eda5ace3a18ed61e9752d78e32828d22b6
Yury <sidorenkoy@restream.rt.ru>
2019-07-07 11:18:30 +0300

    Got rid of jobs queue. Clarified some documentation.

--- jobs/executor.go
+++ jobs/executor.go
@@ -7,11 +7,11 @@ import "fmt"
 
 // Executor has a fixed amount of workers(go-routines) that execute the actual work.
 //
-// Executor accepts simple, yet flexible function (func() {}) that you can enqueue for execution.
+// Executor accepts simple, yet flexible fn (func() {}) that you can enqueue for execution.
 //
-// Enqueued function will eventually be executed.
+// Enqueued fn will eventually be executed.
 // Functions are run in the order they were enqueued.
-// Functions will be executed in parallel if you specify workersAmount to be > 1.
+// Functions will be executed in parallel if you specify workersCnt to be > 1.
 //
 // Make sure that the function you enqueue for execution won't block forever (e.g. writing in channel that won't ever be read from).
 // It will cause the corresponding worker to hang forever - thus leaking resources.
@@ -23,12 +23,10 @@ type Executor struct {
 
 // NewExecutor returns a new Executor object - a means to enqueue your functions for execution.
 //
-// workersAmount - specifies, how many workers(go-routines) Executor will use to handle functions, sent for execution. Executor will run its workers in parallel.
-//
-// queueSize - specifies, how many functions executor can easily hold to (either keeping them in queue or executing them) at any given time.
-func NewExecutor(workersAmount uint, queueSize uint) *Executor {
-	jobQueue := make(chan func(), queueSize)
-	for i := uint(0); i < workersAmount; i++ {
+// workersCnt - specifies, how many workers(go-routines) Executor will use to handle functions, sent for execution. Executor will run its workers in parallel.
+func NewExecutor(workersCnt uint) *Executor {
+	jobQueue := make(chan func())
+	for i := uint(0); i < workersCnt; i++ {
 		consume(jobQueue)
 	}
 	return &Executor{
@@ -36,39 +34,39 @@ func NewExecutor(workersAmount uint, queueSize uint) *Executor {
 	}
 }
 
-// TryToEnqueue is a way for you to schedule a function for execution.
-// Enqueued function will eventually be executed at some point in the future.
+// TryToEnqueue is a way for you to schedule a function fn for execution.
+// Enqueued fn will eventually be executed at some point in the future.
 //
 // TryToEnqueue call doesn't block.
 // TryToEnqueue returns an error if there already are too many functions for Executor to handle at the moment.
-// If TryToEnqueue does return an error, you can try to enqueue your function (and succeed) at some point in the future.
+// If TryToEnqueue does return an error, you can try to enqueue your fn (and succeed) at some point in the future.
 //
-// function - simple Golang function - a unit of work that will be scheduled for execution as soon as there is a free worker to tackle it.
-// function cannot be 'nil'.
+// fn - simple Golang function - a unit of work that will be scheduled for execution as soon as there is a free worker to tackle it.
+// If 'nil' is passed as fn, Executor silently throws it away, as there is nothing to be done.
 //
-// TryToEnqueue is safe to use in multi-threaded environment
-func (executor *Executor) TryToEnqueue(function func()) error {
+// TryToEnqueue is safe to use in multi-threaded environment.
+func (executor *Executor) TryToEnqueue(fn func()) error {
 	if len(executor.jobQueue) == cap(executor.jobQueue) {
 		return fmt.Errorf("executor queue is full at the moment")
 	}
 
-	executor.Enqueue(function)
+	executor.Enqueue(fn)
 
 	return nil
 }
 
-// Enqueue is a way for you to schedule a function for execution.
-// Enqueued function will eventually be executed at some point in the future.
+// Enqueue is a way for you to schedule a function fn for execution.
+// Enqueued fn will eventually be executed at some point in the future.
 //
 // Enqueue call blocks until Executor is ready to accept the function you are trying to enqueue.
 //
-// function - simple Golang function - a unit of work that will be scheduled for execution as soon as there is a free worker to tackle it.
-// function cannot be 'nil'.
+// fn - simple Golang function - a unit of work that will be scheduled for execution as soon as there is a free worker to tackle it.
+// If 'nil' is passed as fn, Executor silently throws it away, as there is nothing to be done.
 //
-// Enqueue is safe to use in multi-threaded environment
-func (executor *Executor) Enqueue(function func()) {
-	if function == nil {
+// Enqueue is safe to use in multi-threaded environment.
+func (executor *Executor) Enqueue(fn func()) {
+	if fn == nil {
 		return
 	}
-	executor.jobQueue <- function
+	executor.jobQueue <- fn
 }
